a246a3b97cc24490e0bb88bd1ac4ec115fd0e258
Hoang Van Duc <duch52362@gmail.com>
2024-08-20 10:35:35 +0700

    Additional Function Delete User And Install Modal-react

--- src/services/userService.js
+++ src/services/userService.js
@@ -4,14 +4,23 @@ const registerNewUser = (data) => {
   return axios.post("http://localhost:8080/api/v1/register", data);
 };
 
-const loginUser =(valueLogin, password) => {
+const loginUser = (valueLogin, password) => {
   return axios.post("http://localhost:8080/api/v1/login", {
-    valueLogin, password
+    valueLogin,
+    password,
   });
-}
+};
 
-const fetchAllUser =(page, limit) => {
-  return axios.get(`http://localhost:8080/api/v1/user/read?page=${page}&limit=${limit}`);
-}
+const fetchAllUser = (page, limit) => {
+  return axios.get(
+    `http://localhost:8080/api/v1/user/read?page=${page}&limit=${limit}`
+  );
+};
 
-export { registerNewUser, loginUser, fetchAllUser };
+const deleteUser = (user) => {
+  return axios.delete("http://localhost:8080/api/v1/user/delete", {
+    data: { id: user.id },
+  });
+};
+
+export { registerNewUser, loginUser, fetchAllUser, deleteUser };
